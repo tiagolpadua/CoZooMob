2ca2e167f928b7087d640546b05877198b9bec33
Tiago Lage Payne de Padua <tiagolpadua@gmail.com>
2019-09-10 16:51:17 -0300

    lista de animais

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,37 @@ const {width} = Dimensions.get('screen');
 
 export default class App extends Component {
   render() {
-    const animal = {
-      nome: 'Leão',
-      urlImagem:
-        'https://upload.wikimedia.org/wikipedia/commons/4/40/Just_one_lion.jpg',
-    };
+    const animais = [
+      {
+        nome: 'Leão',
+        urlImagem:
+          'https://upload.wikimedia.org/wikipedia/commons/4/40/Just_one_lion.jpg',
+      },
+      {
+        nome: 'Girafa',
+        urlImagem:
+          'https://upload.wikimedia.org/wikipedia/commons/9/97/Namibie_Etosha_Girafe_02.jpg',
+      },
+      {
+        nome: 'Gato',
+        urlImagem:
+          'https://upload.wikimedia.org/wikipedia/commons/b/b2/WhiteCat.jpg',
+      },
+    ];
 
     return (
       <View>
-        <Text style={styles.nomeAnimal}>{animal.nome}</Text>
-        <Image
-          source={{
-            uri: animal.urlImagem,
-          }}
-          style={styles.imagemAnimal}
-        />
+        {animais.map(animal => (
+          <View>
+            <Text style={styles.nomeAnimal}>{animal.nome}</Text>
+            <Image
+              source={{
+                uri: animal.urlImagem,
+              }}
+              style={styles.imagemAnimal}
+            />
+          </View>
+        ))}
       </View>
     );
   }
