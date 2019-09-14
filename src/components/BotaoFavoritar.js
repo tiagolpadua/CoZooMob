de9d3e51f2af1561ac7d711fd760e10e41e0cbcc
Tiago Lage Payne de Padua <tiagolpadua@gmail.com>
2019-09-14 18:01:01 -0300

    BotaoFavoritar presentational

--- src/components/BotaoFavoritar.js
+++ src/components/BotaoFavoritar.js
@@ -3,14 +3,6 @@ import React, {Component} from 'react';
 import {TouchableOpacity} from 'react-native';
 
 export default class BotaoFavoritar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      animal: this.props.animal,
-      usuarioLogado: this.props.usuarioLogado,
-    };
-  }
-
   render() {
     const {favoritado, favoritarCallback, desfavoritarCallback} = this.props;
 
