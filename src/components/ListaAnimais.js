9920af2bec1190bddf6d4e4be13d3262d8817370
Tiago Lage Payne de Padua <tiagolpadua@gmail.com>
2019-09-14 16:16:31 -0300

    layout 1

--- src/components/ListaAnimais.js
+++ src/components/ListaAnimais.js
@@ -1,48 +1,49 @@
+import {Container, Content, Header, Title, View} from 'native-base';
 import React, {Component} from 'react';
-import {FlatList, View} from 'react-native';
+import {FlatList, StyleSheet} from 'react-native';
 import Animal from './Animal';
+import {
+  animais as animaisData,
+  usuarioLogado as usuarioLogadoData,
+} from '../../data.json';
 
 export default class ListaAnimais extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      animais: [
-        {
-          nome: 'Leão',
-          urlImagem:
-            'https://upload.wikimedia.org/wikipedia/commons/4/40/Just_one_lion.jpg',
-          favoritoUsuarios: [],
-        },
-        {
-          nome: 'Girafa',
-          urlImagem:
-            'https://upload.wikimedia.org/wikipedia/commons/9/97/Namibie_Etosha_Girafe_02.jpg',
-          favoritoUsuarios: ['maria'],
-        },
-        {
-          nome: 'Gato',
-          urlImagem:
-            'https://upload.wikimedia.org/wikipedia/commons/b/b2/WhiteCat.jpg',
-          favoritoUsuarios: ['maria', 'paulo'],
-        },
-      ],
-      usuarioLogado: 'jose',
+      animais: animaisData,
+      usuarioLogado: usuarioLogadoData,
     };
   }
 
   render() {
     const {animais, usuarioLogado} = this.state;
     return (
-      <View>
-        <FlatList
-          data={animais}
-          renderItem={({item}) => (
-            <Animal animal={item} usuarioLogado={usuarioLogado} />
-          )}
-          keyExtractor={item => item.nome}
-        />
-      </View>
+      <Container>
+        <Header style={styles.header}>
+          <Title>Controle de Animais</Title>
+        </Header>
+        <Content>
+          <FlatList
+            data={animais}
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
+            renderItem={({item}) => (
+              <Animal animal={item} usuarioLogado={usuarioLogado} />
+            )}
+            keyExtractor={item => item.nome}
+          />
+        </Content>
+      </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {height: 30},
+  separator: {
+    height: 1,
+    backgroundColor: '#CED0CE',
+    marginBottom: 10,
+  },
+});
