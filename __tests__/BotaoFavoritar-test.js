a83cf9fc571cc8bb0888c352653694930d4597ae
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2019-09-27 15:50:49 -0300

    teste2

--- __tests__/BotaoFavoritar-test.js
+++ __tests__/BotaoFavoritar-test.js
@@ -9,6 +9,28 @@ import BotaoFavoritar from '../src/components/BotaoFavoritar';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<BotaoFavoritar />);
+it('renderiza se favoritado é true', () => {
+  const tree = renderer
+    .create(
+      <BotaoFavoritar
+        favoritado={true}
+        favoritarCallback={() => {}}
+        desfavoritarCallback={() => {}}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renderiza se favoritado é false', () => {
+  const tree = renderer
+    .create(
+      <BotaoFavoritar
+        favoritado={false}
+        favoritarCallback={() => {}}
+        desfavoritarCallback={() => {}}
+      />,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
